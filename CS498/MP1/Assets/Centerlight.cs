2ed9878657a6792f9f6ada6335f43705c17cc6d8
jeremy-sullivan192 <darkdude192@gmail.com>
2015-09-15 20:47:44 -0500

    almost done with MP1

---
+++ CS498/MP1/Assets/Centerlight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Centerlight : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		print (gameObject.transform.position); 
+		if (gameObject.transform.position.x > 0)
+			transform.Find ("Centerlight").intensity = 0;
+		else 
+			transform.Find ("Centerlight").intensity = 40; 
+	}
+}
---
+++ CS498/MP1/Assets/Lightswitch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightswitch : MonoBehaviour {
+		public Light centerlight;
+
+	void Start () {
+		centerlight = GetComponent <Light>(); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (centerlight.intensity == 0)
+				centerlight.intensity = 5;
+			else 
+				centerlight.intensity = 0;
+		}
+	}
+}
